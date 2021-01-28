48c5678f612f2fca458ee742cccc509cbf5ae9cb
Zoltan Altfatter <altfatterz@gmail.com>
2021-01-28 17:35:58 +0100

    works with @Retryable

--- spring-rabbit-tutorials/async-demo/bar-service/src/main/java/com/example/barservice/RetryableBarService.java
+++ spring-rabbit-tutorials/async-demo/bar-service/src/main/java/com/example/barservice/RetryableBarService.java
@@ -1,22 +1,27 @@
 package com.example.barservice;
 
-import org.springframework.retry.support.RetryTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RetryableBarService {
 
     private BarService barService;
-    private RetryTemplate retryTemplate = RetryTemplate.builder()
-            .maxAttempts(10)
-            .fixedBackoff(1000)
-            .retryOn(CachedBarNotFoundException.class).build();
 
     public RetryableBarService(BarService barService) {
         this.barService = barService;
     }
 
+    @Retryable(value = CachedBarNotFoundException.class, maxAttempts = 10, backoff = @Backoff(500))
     public String getBar(String id)  {
-        return retryTemplate.execute(ctx -> barService.getBar(id));
+        return barService.getBar(id);
+    }
+
+    // Will be executed after we tried the maxAttempts
+    @Recover
+    public String recover(CachedBarNotFoundException e) {
+        return null;
     }
 }
